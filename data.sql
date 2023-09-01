e0850ec0b5e93f14151535a2ceda9210cdd34e5f
PedroH-Neves <PedroHDN710@gmail.com>
2023-09-01 15:18:14 -0400

    Update data file

--- data.sql
+++ data.sql
@@ -1,4 +1,8 @@
 /* Populate database with sample data. */
 
-
+INSERT INTO ANIMALS (NAME,DATE_OF_BIRTH,ESCAPE_ATTEMPTS,NEUTERED,WEIGHT_KG) VALUES 
+('Agumon', '2020-02-03',0, true, 10.23 ), 
+('Gabumon', '2018-11-15',2, true, 8 ), 
+('Pikachu', '2021-01-07',1, false, 15.04 ), 
+('Devimon', '2017-05-12',5, true, 11 );
 
