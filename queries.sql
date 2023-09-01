5dac40eb5e9221becee219713aa84f3176c7db7d
PedroH-Neves <PedroHDN710@gmail.com>
2023-09-01 15:28:08 -0400

    Update queries file

--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-
+SELECT * FROM ANIMALS WHERE NAME LIKE '%mon';
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+SELECT name FROM animals WHERE neutered=true AND escape_attempts < 3;
+SELECT date_of_birth FROM animals WHERE name = 'Pikachu' OR name = 'Agumon';
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutered=true;
+SELECT * FROM animals WHERE name NOt IN ('Gabumon');
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3 OR weight_kg = 17.3 OR weight_kg = 10.4;
